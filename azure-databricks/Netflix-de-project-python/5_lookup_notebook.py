abb7ed38384f93e5c8f1bc2c3dfe8e4d8483282b
Aditya <shanbhag.ad@northeastern.edu>
2025-07-01 20:11:40 -0400

    Updating Azure Databricks files

--- azure-databricks/Netflix-de-project-python/5_lookup_notebook.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-dbutils.widgets.text("weekday", "7")
-
-# COMMAND ----------
-
-var = int(dbutils.widgets.get("weekday"))
-dbutils.jobs.taskValues.set(key="weekoutput", value = var)
--- azure-databricks/Netflix-de-project-python/6_Lookup_days_count.py
+++
@@ -1,6 +0,0 @@
-# Databricks notebook source
-var1 = dbutils.jobs.taskValues.get(taskKey="Weekday_lookup", key="weekoutput")
-
-# COMMAND ----------
-
-print(var1)
